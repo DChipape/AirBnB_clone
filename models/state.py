07676b959c707123b7f7808fef56ae005a1fd343
DCHIPAPE <dimakatsosharon94@gmail.com>
2023-10-17 09:11:43 -0700

    our project

---
+++ models/state.py
@@ -0,0 +1,27 @@
+tate class"""
+
+from models.base_model import BaseModel
+
+
+
+
+
+class State(BaseModel):
+
+        """Represent a state and inherits from the BaseModel.
+
+
+
+            Attributes:
+
+                        name (str): The name of the state.
+
+                            """
+
+
+
+                                name = ""
+
+
+
+
